1621b7adac544a4ae491f2ac96248ca7ec86ee05
Chinna Ch <Chinna Ch>
2017-07-21 17:24:18 +0530

    Initial commit

---
+++ app/src/main/java/com/smcc/application/StartingActivity.java
@@ -0,0 +1,27 @@
+package com.smcc.application;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartingActivity extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.starting_gif);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(StartingActivity.this,LoginTypes.class);
+                startActivity(intent);
+
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
