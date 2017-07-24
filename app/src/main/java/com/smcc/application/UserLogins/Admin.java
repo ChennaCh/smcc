2aa36d619bd571565d07f441cdb1f0913e032746
ChennaCh <chennarao07@gmail.com>
2017-07-24 15:42:39 +0530

    xml

---
+++ app/src/main/java/com/smcc/application/UserLogins/Admin.java
@@ -0,0 +1,15 @@
+package com.smcc.application.UserLogins;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.smcc.application.R;
+
+public class Admin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
