58e389f8cd2b832f9ed520c9d543b284cce9240a
smcc <chennarao07@gmail.com>
2017-08-18 15:13:56 +0530

    Acadamics and placements

---
+++ app/src/main/java/com/smcc/application/Activity/Research.java
@@ -0,0 +1,16 @@
+package com.smcc.application.Activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.smcc.application.R;
+
+public class Research extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_research);
+    }
+
+}
